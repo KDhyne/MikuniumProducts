2b2f7db2a1721f284208edff9781ade62edb2dbe
KDhynamo <kdhynamo@gmail.com>
2025-01-22 13:22:19 -0500

    Add fission reactor equipment graphics

--- commons.lua
+++ commons.lua
@@ -5,29 +5,32 @@ mikuColor = { 0.71, 0.902, 0.953 }
 
 replacementPairs = {
 
-    { "ammo",               "atomic-bomb" },
-    { "ammo",               "explosive-uranium-cannon-shell" },
-    { "ammo",               "uranium-cannon-shell" },
-    { "ammo",               "uranium-rounds-magazine" },
+    { "ammo",                "atomic-bomb" },
+    { "ammo",                "explosive-uranium-cannon-shell" },
+    { "ammo",                "uranium-cannon-shell" },
+    { "ammo",                "uranium-rounds-magazine" },
 
-    { "assembling-machine", "centrifuge" },
+    { "assembling-machine",  "centrifuge" },
 
-    { "item",               "centrifuge" },
-    { "item",               "depleted-uranium-fuel-cell" },
-    { "item",               "nuclear-fuel" },
-    { "item",               "uranium-235" },
-    { "item",               "uranium-238" },
-    { "item",               "uranium-fuel-cell" },
+    { "item",                "centrifuge" },
+    { "item",                "depleted-uranium-fuel-cell" },
+    { "item",                "nuclear-fuel" },
+    { "item",                "uranium-235" },
+    { "item",                "uranium-238" },
+    { "item",                "uranium-fuel-cell" },
 
-    { "technology",         "atomic-bomb" },
-    { "technology",         "kovarex-enrichment-process" },
-    { "technology",         "nuclear-fuel-reprocessing" },
-    { "technology",         "uranium-ammo" },
-    { "technology",         "uranium-mining" },
-    { "technology",         "uranium-processing" },
+    { "technology",          "atomic-bomb" },
+    { "technology",          "kovarex-enrichment-process" },
+    { "technology",          "nuclear-fuel-reprocessing" },
+    { "technology",          "fission-reactor-equipment" },
+    { "technology",          "uranium-ammo" },
+    { "technology",          "uranium-mining" },
+    { "technology",          "uranium-processing" },
 
 
-    { "recipe",             "kovarex-enrichment-process" },
-    { "recipe",             "nuclear-fuel-reprocessing" },
-    { "recipe",             "uranium-processing" },
+    { "recipe",              "kovarex-enrichment-process" },
+    { "recipe",              "nuclear-fuel-reprocessing" },
+    { "recipe",              "uranium-processing" },
+
+    { "generator-equipment", "fission-reactor-equipment" },
 }
--- prototypes/mikunium-retexture.lua
+++ prototypes/mikunium-retexture.lua
@@ -1,7 +1,7 @@
 require("commons")
 
--- Gets new textures by replacing the base root folder with the mod root folder
-local function getNewTexturePath(oldPath)
+-- Gets new file paths by replacing the base root folder with the mod root folder
+local function getNewModPath(oldPath)
     local newPath = string.gsub(oldPath, "^__base__", modRoot)
     return newPath
 end
@@ -10,12 +10,12 @@ end
 local function replacePrototypeTexture(prototypeType, internalName)
     local prototype = data.raw[prototypeType][internalName]
     if prototype then
-        prototype.icon = getNewTexturePath(prototype.icon)
+        prototype.icon = getNewModPath(prototype.icon)
 
         -- Replace item varations for belts/ground
         if prototype.pictures and prototype.pictures.layers then
             for _, layer in ipairs(prototype.pictures.layers) do
-                layer.filename = getNewTexturePath(layer.filename)
+                layer.filename = getNewModPath(layer.filename)
             end
         end
     end
@@ -31,7 +31,7 @@ end
 
 local mikuOreItem = data.raw["item"]["uranium-ore"]
 if mikuOreItem then
-    mikuOreItem.icon = getNewTexturePath(mikuOreItem.icon)
+    mikuOreItem.icon = getNewModPath(mikuOreItem.icon)
     mikuOreItem.icon_size = 64
 
     -- Replace ore variations for belts/ground
@@ -40,10 +40,10 @@ if mikuOreItem then
         for _, pic in ipairs(pics) do
             if pic.layers then
                 for _, layer in ipairs(pic.layers) do
-                    layer.filename = getNewTexturePath(layer.filename)
+                    layer.filename = getNewModPath(layer.filename)
                 end
             else
-                pic.filename = getNewTexturePath(pic.filename)
+                pic.filename = getNewModPath(pic.filename)
             end
         end
     end
@@ -53,14 +53,14 @@ end
 
 local oreResource = data.raw["resource"]["uranium-ore"]
 if oreResource then
-    oreResource.icon = getNewTexturePath(oreResource.icon)
+    oreResource.icon = getNewModPath(oreResource.icon)
     oreResource.map_color = mikuColor
     oreResource.mining_visualisation_tint = mikuColor
     local oreResourceSheet = oreResource.stages.sheet
-    oreResourceSheet.filename = getNewTexturePath(oreResourceSheet.filename)
+    oreResourceSheet.filename = getNewModPath(oreResourceSheet.filename)
     if oreResource.stages_effect then
         local oreResourceEffectSheet = oreResource.stages_effect.sheet
-        oreResourceEffectSheet.filename = getNewTexturePath(oreResourceEffectSheet.filename)
+        oreResourceEffectSheet.filename = getNewModPath(oreResourceEffectSheet.filename)
     end
 end
 
@@ -70,8 +70,16 @@ local centrifugeEntity = data.raw["assembling-machine"]["centrifuge"]
 if centrifugeEntity then
     local lights = centrifugeEntity["graphics_set"]["working_visualisations"][2].animation.layers
     for _, light in ipairs(lights) do
-        light.filename = getNewTexturePath(light.filename)
+        light.filename = getNewModPath(light.filename)
     end
+    -- Was gonna change this too but I think hearing "popipo" on staggered loop 40 times at once would drive me insane
+    -- Maybe I'll add it later as an option
+    -- local workingSound = centrifugeEntity.working_sound
+    -- if workingSound.sound then
+    --     for _, varation in ipairs(workingSound.sound) do
+    --         varation.filename = getNewModPath(varation.filename)
+    --     end
+    -- end
 end
 
 -- Change the color of the rocket tip for nukes when shot from the rocket launcher
@@ -82,22 +90,26 @@ if atomicRocketProjectile then
         mikuColor)
 end
 
+-- Change color of nuke explosion
+
 local nukeExplosion = data.raw["explosion"]["nuke-explosion"]
 if nukeExplosion then
     local stripes = nukeExplosion["animations"]["stripes"]
     if stripes then
         for _, stripe in ipairs(stripes) do
-            stripe.filename = getNewTexturePath(stripe.filename)
+            stripe.filename = getNewModPath(stripe.filename)
         end
     end
 end
+
+-- Change color of nuke shockwave
 
 local nukeShockwave = data.raw["explosion"]["atomic-nuke-shockwave"]
 if nukeShockwave then
     local animations = nukeShockwave["animations"]
     if animations then
         for _, anim in ipairs(animations) do
-            anim.filename = getNewTexturePath(anim.filename)
+            anim.filename = getNewModPath(anim.filename)
         end
     end
 end
